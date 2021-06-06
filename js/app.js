a89c047d60ffe757a2984741d984bc00511bf67b
nemeh-abuzitoun <nemehjaaaaser@gmail.com>
2021-06-06 17:50:11 +0300

    added a js and a css  for  the html structure  of  gitting up and running

---
+++ js/app.js
@@ -0,0 +1,15 @@
+let userName = prompt(please enter your Name!');
+console.log(userName);
+alert('welcome'+userName+'to our web site');
+
+let color = prompt(what is your favourite color!');
+console.log(color);
+alert('beautiful choice');
+
+let weight = prompt(what is your weight!');
+console.log(weight);
+alert('stay healthy all the time');
+
+let hoppy = prompt(what do you do in your free time as a hoppy!');
+console.log(hoppy);
+alert('nice one we should try it too ');
