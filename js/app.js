0e81b216bd95e193f59f47c42ba833021939cf80
nemeh-abuzitoun <nemehjaaaaser@gmail.com>
2021-07-02 21:36:14 +0300

    fixing and practicing

--- js/app.js
+++ js/app.js
@@ -1,15 +1,15 @@
-let userName = prompt('please enter your Name!');
-console.log(userName);
-alert('welcome'+userName+'to our web site');
+'user strict,'
 
+let userName = prompt ('please enter your Name!');
+console.log(userName);  
+alert('welcome' + userName + 'to our web site');
 let color = prompt('what is your favourite color!');
 console.log(color);
 alert('beautiful choice');
 
-let weight = prompt('what is your weight!');
+let weight = ('prompt(what is your weight!');
 console.log(weight);
 alert('stay healthy all the time');
-
 let hoppy = prompt('what do you do in your free time as a hoppy!');
 console.log(hoppy);
-alert('nice one we should try it too');
+alert('nice one we should try it too ');
